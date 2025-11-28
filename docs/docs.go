e96f1e9a4cdafd7eef280f9fda922d846bc465be
mamat1815 <mbahgendengngegame@gmail.com>
2025-11-28 23:27:52 +0700

    docs: add generated Swagger API documentation for asset management endpoints.

--- docs/docs.go
+++ docs/docs.go
@@ -1684,7 +1684,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "http://157.10.161.213:3000",
 	BasePath:         "/api/v1",
 	Schemes:          []string{},
 	Title:            "Campus Lost & Found API",
