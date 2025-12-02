1a750fded41e5b59d846a49bc723b3ff2e8e86dc
mamat1815 <mbahgendengngegame@gmail.com>
2025-12-02 10:45:10 +0700

    feat: update API host in main.go to new domain

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -29,7 +29,7 @@ import (
 // @license.name Apache 2.0
 // @license.url http://www.apache.org/licenses/LICENSE-2.0.html
 
-// @host 157.10.161.213:3000
+// @host api.afsar.my.id
 // @BasePath /api/v1
 // @securityDefinitions.apikey BearerAuth
 // @in header
